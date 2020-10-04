3fa8a6a2690139c37fd93e78e7d043c906b84cfd
Matthew Knight <mattknight.sa@gmail.com>
2020-10-04 12:05:02 -0500

    created sql query

--- acquire.py
+++ acquire.py
@@ -6,3 +6,9 @@ import os
 
 def get_connection(db, user=user, host=host, password=password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+def new_zillow_data():
+    sql_query =  "SELECT properties_2017.parcelid, structuretaxvaluedollarcnt, taxvaluedollarcnt, landtaxvaluedollarcnt, taxamount, transactiondate FROM properties_2017 JOIN predictions_2017 on predictions_2017.parcelid = properties_2017.parcelid WHERE unitcnt = 1 AND transactiondate BETWEEN '2017-05-01' AND '2017-06-30'"
+    df = pd.read_sql(sql_query, get_connection('zillow'))
+    df.to_csv('zillow.csv')
+    return df
